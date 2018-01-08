c9331dc6f4b9fb745dd9223abe9272f0e8c591a0
chibike <chibuikepraise@gmail.com>
2018-01-08 17:03:53 +0000

    testing...

--- src/image_processing_pkg/scripts/processed_image_server.py
+++ src/image_processing_pkg/scripts/processed_image_server.py
@@ -10,17 +10,8 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from flask import Flask, Response, request, abort, render_template_string, send_from_directory
 
-class EndpointAction(object):
-    def __init__(self, action):
-        self.action = action
-        self.response = Response(status=200, headers={})
-
-    def __call__(self, *args):
-        return self.action()
-
+app = Flask(name)
 class ProcessedImageServer(object):
-    app = None
-
     """docstring for ProcessedImageServer"""
     def __init__(self, name, host="localhost", port="5000"):
         super(ProcessedImageServer, self).__init__()
@@ -28,9 +19,7 @@ class ProcessedImageServer(object):
         
         self.host = host
         self.port = port
-        self.app = Flask(name)
-        self.app.add_url_rule('/', 'test', EndpointAction(self.test))
-        self.app.add_url_rule('/image', 'image', EndpointAction(self.get_image))
+        self.app = app
 
         self.bridge = CvBridge()
         self.processed_image = None
@@ -42,9 +31,11 @@ class ProcessedImageServer(object):
         rospy.init_node('processed_image_server', anonymous=False)
         rospy.Subscriber("processed_image", Image, self.callback)
 
+    @app.route("/")
     def test(self):
         return "Hello!"
 
+    @app.route("/image")
     def get_image(self):
         if self.processed_image:
             rospy.loginfo("Fetching processed image")
