d40c919e501390d5c517e90d617793d900e1f856
Chibuike Praise <chibuikepraise@gmail.com>
2018-02-27 21:46:01 +0000

    added speech file

--- src/nodejs_app_pkg/npm_directory/scripts/audio_man.js
+++ src/nodejs_app_pkg/npm_directory/scripts/audio_man.js
@@ -7,17 +7,21 @@ class Audio_Man
 	{
 		this.player = require('play-sound')();//(opts = {mplayer});
 		this.music_directory = "/home/odroid/artbot_ws/src/nodejs_app_pkg/npm_directory/music/";
-		this.audio_files = ["himitsu.mp3"];
+		this.audio_files = ["ukulele.mp3", "breeze.mp3", "buddy.mp3", "happyrock.mp3", "himitsu.mp3"];
+
+		this.speech_directory = "/home/odroid/artbot_ws/src/nodejs_app_pkg/npm_directory/speech/";
+		this.speech_files = ["test_speech.wav"];
 		
+		this.speech = undefined;
 		this.audio = undefined;
-		this.current_index = undefined;
+		this.current_index = 0;
 	}
 
 	start()
 	{
 		this.stop();
 		var audio_file = this.music_directory + this.audio_files[this.current_index];
-		this.audio = this.player.play(audio_file, this.error_handler);
+		this.play(audio_file);
 	}
 
 	stop()
@@ -26,24 +30,57 @@ class Audio_Man
 		{
 			this.audio.kill();
 		}
+	}
 
-		this.current_index = 0;
+	stop_speech()
+	{
+		if (this.speech)
+		{
+			this.speech.kill();
+		}
+	}
+
+	stop_all()
+	{
+		this.stop();
+		this.stop_speech();
+	}
+
+
+	play_speech(index)
+	{
+		index = index % this.speech_files.length;
+		var audio_file = this.speech_directory + this.speech_files[index];
+		
+		this.speech = this.player.play(audio_file, this.error_handler);
+	}
+
+	play(audio_file)
+	{
+		this.audio = this.player.play(audio_file, this.error_handler);
+		console.log(this.audio);
 	}
 
 	pause()
 	{
-
 	}
 
 	continue()
 	{
+	}
 
+	next()
+	{
+		this.stop();
+		this.current_index = (this.current_index + 1) % this.audio_files.length;
+		var audio_file = this.music_directory + this.audio_files[this.current_index];
+		this.play(audio_file);
 	}
 
 	error_handler(err)
 	{
 		console.log("audio_error", err);
-		if (err && !err.killed) throw err
+		// if (err && !err.killed) throw err
 	}
 }
 
